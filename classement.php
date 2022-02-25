8151df91cbf6c809740fef441c16301b50fe8cbc
tatanox <gallandludinard.a@gmail.com>
2022-02-25 01:32:16 +0100

    push-pre-oubli

--- classement.php
+++ classement.php
@@ -8,10 +8,43 @@
     <link rel="stylesheet" href="./assets/css/classement.css">
 </head>
 <body>
-    <div class="blueberry-kush"></div>
+    <div class="blueberry-kush">
+        <h1 class="orange-bud"> Classement </h1>
+    </div>
     <div class="lemon-haze">
         <div class="white-widow"></div>
-        <div class="amnesia-haze"></div>
+        <div class="amnesia-haze">
+
+<table class="tftable" border="1">
+<tr><th> Rang  </th><th> Pays           </th><th> Dirigeant(e)                  </th><th> Puissance Militaire  </th><th> Ressources </th></tr>
+<tr><td> #1    </td><td class="tangerine-g13"> NZ  <div class="belladonna"><img src="./assets/img/1F1F3-1F1FF_color.png" alt="New Zeland" class="acapulco-gold"> </div> </td><td> Tampon (Romain)               </td><td> 50 000               </td><td> 45 000     </td></tr>
+<tr><td> #2    </td><td class="tangerine-g13"> DE  <div class="belladonna"><img src="./assets/img/1F1E9-1F1EA_color.png" alt="Germany" class="acapulco-gold"> </div> </td><td> M.Genonceau (Julien)          </td><td> 40 200               </td><td> 43 200     </td></tr>
+<tr><td> #3    </td><td class="tangerine-g13"> ES  <div class="belladonna"><img src="./assets/img/1F1EA-1F1F8_color.png" alt="Spain" class="acapulco-gold"> </div> </td><td> Pyyxel (Axel)                 </td><td> 38 100               </td><td> 44 000     </td></tr>
+<tr><td> #4    </td><td class="tangerine-g13"> FR  <div class="belladonna"><img src="./assets/img/1F1EB-1F1F7_color.png" alt="France" class="acapulco-gold"> </div> </td><td> Tetris (Jordan)               </td><td> 43 200               </td><td> 40 200     </td></tr>
+<tr><td> #5    </td><td class="tangerine-g13"> NL  <div class="belladonna"><img src="./assets/img/1F1F3-1F1F1_color.png" alt="Netherlands" class="acapulco-gold"> </div> </td><td> KannaGames (Steven)           </td><td> 36 400               </td><td> 38 100     </td></tr>
+<tr><td> #6    </td><td class="tangerine-g13"> SE  <div class="belladonna"><img src="./assets/img/1F1F8-1F1EA_color.png" alt="Sweden" class="acapulco-gold"> </div> </td><td> Super_Guigui (Guillaume)      </td><td> 40 200               </td><td> 39 000     </td></tr>
+<tr><td> #7    </td><td class="tangerine-g13"> BE  <div class="belladonna"><img src="./assets/img/1F1E7-1F1EA_color.png" alt="Seumland" class="acapulco-gold"> </div> </td><td> JJJ (JJ)                      </td><td> 32 100               </td><td> 36 800     </td></tr>
+<tr><td> #8    </td><td class="tangerine-g13"> IR  <div class="belladonna"><img src="./assets/img/1F1EE-1F1F7_color.png" alt="Iran" class="acapulco-gold"> </div> </td><td> Hisoka (Ted B)                </td><td> 36 700               </td><td> 31 200     </td></tr>
+<tr><td> #9    </td><td class="tangerine-g13"> BR  <div class="belladonna"><img src="./assets/img/1F1E7-1F1F7_color.png" alt="Brazil" class="acapulco-gold"> </div> </td><td> Xtrem (Nina)                  </td><td> 28 300               </td><td> 29 100     </td></tr>
+<tr><td> #10   </td><td class="tangerine-g13"> GB  <div class="belladonna"><img src="./assets/img/1F1EC-1F1E7_color.png" alt="United Kingdom" class="acapulco-gold"> </div> </td><td> Metheno (Teddy H.)            </td><td> 29 800               </td><td> 28 000     </td></tr>
+<tr><td> #11   </td><td class="tangerine-g13"> KR  <div class="belladonna"><img src="./assets/img/1F1F0-1F1F7_color.png" alt="South Korea" class="acapulco-gold"> </div> </td><td> Erasedon (Yanis)              </td><td> 29 200               </td><td> 29 800     </td></tr>
+<tr><td> #12   </td><td class="tangerine-g13"> SA  <div class="belladonna"><img src="./assets/img/1F1F8-1F1E6_color.png" alt="Saudi Arabia" class="acapulco-gold"> </div> </td><td> Tintindrumz (Quentin)         </td><td> 28 000               </td><td> 29 200     </td></tr>
+<tr><td> #13   </td><td class="tangerine-g13"> JP  <div class="belladonna"><img src="./assets/img/1F1EF-1F1F5_color.png" alt="Japan" class="acapulco-gold"> </div> </td><td> Bakasta (Antoine)             </td><td> 29 800               </td><td> 27 100     </td></tr>
+<tr><td> #14   </td><td class="tangerine-g13"> CA  <div class="belladonna"><img src="./assets/img/1F3F4-E0063-E0061-E0071-E0063-E007F_color.png" alt="Canada" class="acapulco-gold mango-kush"> </div> </td><td> Epyion (Chris)                </td><td> 23 200               </td><td> 21 000     </td></tr>
+<tr><td> #15   </td><td class="tangerine-g13"> PL  <div class="belladonna"><img src="./assets/img/1F1F5-1F1F1_color.png" alt="Poland" class="acapulco-gold"> </div> </td><td> Badboo_69xX (Kevin C.)        </td><td> 23 100               </td><td> 28 800     </td></tr>
+<tr><td> #16   </td><td class="tangerine-g13"> AU  <div class="belladonna"><img src="./assets/img/1F1E6-1F1FA_color.png" alt="Australia" class="acapulco-gold"> </div> </td><td> Vegeta (Mika)                 </td><td> 21 000               </td><td> 23 200     </td></tr>
+<tr><td> #17   </td><td class="tangerine-g13"> US  <div class="belladonna"><img src="./assets/img/1F1FA-1F1F8_color.png" alt="United States" class="acapulco-gold"> </div> </td><td> FBI (Joannie)                 </td><td> 22 000               </td><td> 21 100     </td></tr>
+<tr><td> #18   </td><td class="tangerine-g13"> AR  <div class="belladonna"><img src="./assets/img/1F1E6-1F1F9_color.png" alt="Austria" class="acapulco-gold"> </div> </td><td> HassanK (Hassan)              </td><td> 19 900               </td><td> 22 000     </td></tr>
+<tr><td> #19   </td><td class="tangerine-g13"> CN  <div class="belladonna"><img src="./assets/img/1F1E8-1F1F3_color.png" alt="China" class="acapulco-gold"> </div> </td><td> Zalando (Kylian)              </td><td> 19 100               </td><td> 22 800     </td></tr>
+<tr><td> #20   </td><td class="tangerine-g13"> MX  <div class="belladonna"><img src="./assets/img/1F1F2-1F1FD_color.png" alt="Mexico" class="acapulco-gold"> </div> </td><td> Baxter (J-P)                  </td><td> 17 800               </td><td> 20 200     </td></tr>
+<tr><td> #21   </td><td class="tangerine-g13"> TR  <div class="belladonna"><img src="./assets/img/1F1F9-1F1F7_color.png" alt="Turkey" class="acapulco-gold"> </div> </td><td> Lynala (Cedric)               </td><td> 22 800               </td><td> 19 100     </td></tr>
+<tr><td> #22   </td><td class="tangerine-g13"> IE  <div class="belladonna"><img src="./assets/img/1F1EE-1F1EA_color.png" alt="Ireland" class="acapulco-gold"> </div> </td><td> Goku (Kevin)                  </td><td> 19 100               </td><td> 18 000     </td></tr>
+<tr><td> #23   </td><td class="tangerine-g13"> TH  <div class="belladonna"><img src="./assets/img/1F1F9-1F1ED_color.png" alt="Thailand" class="acapulco-gold"> </div> </td><td> Darksider21 (Erwan)           </td><td> 16 800               </td><td> 17 800     </td></tr>
+<tr><td> #24   </td><td class="tangerine-g13"> NG  <div class="belladonna"><img src="./assets/img/1F1F3-1F1EC_color.png" alt="Nigeria" class="acapulco-gold"> </div> </td><td> Monster_pandaroux08 (Yannick) </td><td> 18 000               </td><td> 15 200     </td></tr>
+<tr><td> #25   </td><td class="tangerine-g13"> CH  <div class="belladonna"><img src="./assets/img/1F1E8-1F1ED_color.png" alt="Switzerland" class="acapulco-gold"> </div> </td><td> Orchidoclaste (Anthony)       </td><td> 1 001                </td><td> 3 100      </td></tr>
+<tr><td> #26   </td><td class="tangerine-g13"> DZ  <div class="belladonna"><img src="./assets/img/1F1E9-1F1FF_color.png" alt="Algeria" class="acapulco-gold"> </div> </td><td> XxAndyLeBgDu08480xX (Andy)    </td><td> 1 000                </td><td> 2 000      </td></tr>
+</table>
+        </div>
     </div>
 </body>
 </html>
