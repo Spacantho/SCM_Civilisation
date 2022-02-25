0f0b7cc63b2d1ed2fb894510fd44a618fce06942
Spacantho <98018883+Spacantho@users.noreply.github.com>
2022-02-25 12:02:08 +0100

    Update navbar.php

--- assets/includes/navbar.php
+++ assets/includes/navbar.php
@@ -1,16 +1,3 @@
-<!DOCTYPE html>
-<html lang="en">
-
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link href="./assets/css/includes.css" rel="stylesheet">
-    <title>Document</title>
-</head>
-
-<body>
-
     <nav>
 
         <div class="nav-menug">
@@ -55,6 +42,3 @@
 
     </nav>
     <script src="assets/js/includes.js"></script>
-</body>
-
-</html>
